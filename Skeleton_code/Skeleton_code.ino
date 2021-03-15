cb4d0442e1141e0cfa731fe7956354b231cfa9a3
tuanphan12 <phantuan06@gmail.com>
2021-03-15 02:14:07 -0500

    got the accel to work

--- Skeleton_code/Skeleton_code.ino
+++ Skeleton_code/Skeleton_code.ino
@@ -1,9 +1,36 @@
+#include "MPU9250.h"
+
+MPU9250 IMU(0x68, 0);
+
+float ax, ay, az, t;
+int beginStatus;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //start communication with the IMU
+    beginStatus = IMU.begin(ACCEL_RANGE_2G,GYRO_RANGE_250DPS);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if(beginStatus < 0) {
+    delay(1000);
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    delay(5000);
+  }
+   else{
+    IMU.getAccel(&ax, &ay, &az);      //there are functions to get both the accel and gyro at the same time
+    IMU.getTemp(&t);                  //that would be better to represent motion since there are no time lag  
 
+    Serial.print(ax,6);
+    Serial.print("\t");
+    Serial.print(ay,6);
+    Serial.print("\t");
+    Serial.print(az,6);
+    Serial.print("\t");
+    Serial.println(t,6);
+    delay(50);
+  }
 }
